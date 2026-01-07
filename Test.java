6fdd34ef1fcc11eb1fd3a9772687020a79bf4c46
PhmLng <phamdacluong04@gmail.com>
2026-01-07 16:57:01 +0700

    Add Test.java

---
+++ Test.java
@@ -0,0 +1,29 @@
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student("vgs1422", "Nguyen Van A", 3.6);
+        System.out.println("Name: ");
+        student.displayStudentInfor();
+    }
+}
+
+class Student {
+    private String id;
+    private String name;
+    private double gpa;
+
+    public Student() {
+        super();
+    }
+
+    public Student(String id, String name, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public void displayStudentInfor() {
+        System.out.println("Id : " + this.id);
+        System.out.println("Name: " + this.name);
+        System.out.println("Gpa: " + this.gpa);
+    }
+}
